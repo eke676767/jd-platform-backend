340f7f30b3fa1934ce6ac5a51dbdd8dbc0f671d2
DESKTOP-3JVAMH0\dt <eke6767@naver.com>
2026-02-28 13:53:34 +0900

    first commit

---
+++ src/main/java/com/jdcompany/platform/JdPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.jdcompany.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdPlatformApplication.class, args);
+	}
+
+}
